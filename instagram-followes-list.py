a5ce83aeb6eadfeb6d795239e52369fccc948d45
Kurabiyem <mehmetali_buber@hotmail.com>
2020-12-18 12:41:26 +0300

    Update instagram-followes-list.py

--- instagram-followes-list.py
+++ instagram-followes-list.py
@@ -28,8 +28,8 @@ try:
 except:
     print("webdriver error")
     
-username = "vali49129@gmail.com"
-password = "qwerty12345"    
+username = "xxxx@gmail.com"
+password = "xxxxx"    
 
 browser.get("https://www.instagram.com/")
 
